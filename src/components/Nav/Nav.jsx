1b4cccae508e5f18c30e87e43740a005bcdbb370
Richardxugit <cansolion@163.com>
2020-06-16 13:48:00 +1000

    initial

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Nav.module.scss";
+import Siderbar from "./components/SiderBar"
 
 const Nav = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.sideBar}>
+        < Siderbar/>
       </div>
     </div>
   );
---
+++ src/components/Nav/components/SiderBar/Siderbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Siderbar = () => {
+  return (
+    <div >
+    test
+    </div>
+  );
+}
+
+export default Siderbar;
