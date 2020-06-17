3c7e69df54ebb70584f7b60c4811f86f191c7a36
Richardxugit <cansolion@163.com>
2020-06-17 14:08:41 +1000

    structure for nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Nav.module.scss";
-import Siderbar from "./components/SiderBar"
+import Sidebar from "./components/SideBar";
+
 
 const Nav = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.sideBar}>
-        < Siderbar/>
+        < Sidebar/>       
       </div>
+  
     </div>
   );
 }
