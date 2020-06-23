af2914bfd35044d4359ef6dc512b1f7d6098ce70
Richardxugit <cansolion@163.com>
2020-06-23 17:55:04 +1000

    static page for login

---
+++ src/components/Login/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./LoginForm.module.scss";
+
+class LoginForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render(){
+        return (
+          <div>
+            <div className={styles.modal}>
+                <form className={styles.form}>
+                  <h3 className={styles.title}>login</h3>
+                  <div className={styles.control}>
+                    <label >UserID</label>
+                    <input type="text" placeholder="Enter UserID" />
+                  </div>
+                  <div className={styles.control}>
+                    <label className={styles.label}>Password</label>
+                    <input className={styles.input} type="text" placeholder="Enter Password" />
+                  </div>
+                  <button className={styles.button} onClick={this.props.toggle}>Submit</button>
+                </form>
+            </div>
+            <div className={styles.mask}></div>
+          </div>
+        );
+      }
+    }
+
+export default LoginForm;
