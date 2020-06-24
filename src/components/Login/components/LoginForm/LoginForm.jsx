20489a9b7c7914065e25d741a01afa0c57df7028
unknown <c2f2020b@gmail.com>
2020-06-24 14:41:39 +1000

    fix browser incompatible

--- src/components/Login/components/LoginForm/LoginForm.jsx
+++ src/components/Login/components/LoginForm/LoginForm.jsx
@@ -14,11 +14,11 @@ class LoginForm extends React.Component{
                   <h3 className={styles.title}>login</h3>
                   <div className={styles.control}>
                     <label >UserID</label>
-                    <input type="text" placeholder="Enter UserID" />
+                    <input type="text" placeholder="Enter UserID"/>
                   </div>
                   <div className={styles.control}>
                     <label className={styles.label}>Password</label>
-                    <input className={styles.input} type="text" placeholder="Enter Password" />
+                    <input className={styles.input} type="text" placeholder="Enter Password"/>
                   </div>
                   <button className={styles.button} onClick={this.props.toggle}>Submit</button>
                 </form>
