32684b99d8edfacd8fa3b877817de5d7d179453a
Richardxugit <cansolion@163.com>
2020-07-02 17:40:37 +1000

    change http

--- src/apis/http-common.js
+++ src/apis/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.0.196:8080/",
+  baseURL: "http://localhost:8080/",
   headers: {
     "Content-type": "application/json"
   }
