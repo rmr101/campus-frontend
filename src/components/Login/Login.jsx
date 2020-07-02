01f42eace1c8cea3d3df2376f1c15beca92e61e8
jackytsheng <jackytsheng@gmail.com>
2020-07-03 01:11:33 +1000

    change style on log in

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,20 +5,41 @@ import { withRouter } from "react-router-dom";
 import ChangeRole from "../../store/authentication/actions/ChangeRole";
 import SetLogin from "../../store/authentication/actions/SetLogin";
 
+// user id : admin   password: admin 
+// user id : S95778487   password: S95778487 Student. 
+// user id : T54346913   password: T54346913 Teacher.div
+
+
+const Greeting = () => {
+const Day = [ "Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+const Month = ["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"]
+const date = new Date();
+return (
+  <div className={styles.greeting}>
+    <h3 className={styles.date}>{`${Day[date.getDay()].toUpperCase()}`}</h3>
+    <h3 className={styles.date}>{`${date.getDate()}, ${Month[
+      date.getMonth()
+    ].toUpperCase()}`}</h3>
+    <h1 className={styles.title}>Hello Friend!</h1>
+    <p>A management system which only makes room for things that matter.</p>
+  </div>
+);
+}
+
 const Login = ({
   onClick,
   history
 }) => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <div className={styles.modal}>
+        <Greeting />
         <form className={styles.form}>
-          <h3 className={styles.title}>login</h3>
-          {/* <div className={styles.control}>
-            <label>UserID</label>
+          <div className={styles.control}>
+            <label>User ID</label>
             <input
               type="text"
-              placeholder="Enter UserID"
+              placeholder="Enter User ID"
               maxLength={30}
               required
             />
@@ -32,7 +53,17 @@ const Login = ({
               maxLength={30}
               required
             />
-          </div> */}
+          </div>
+          <button
+            className={styles.button}
+            to="/"
+            onClick={(e) => {
+              history.push("/");
+              onClick("student");
+            }}
+          >
+            Log in
+          </button>
           <button
             className={styles.button}
             to="/"
@@ -65,7 +96,6 @@ const Login = ({
           </button>
         </form>
       </div>
-      <div className={styles.mask}></div>
     </div>
   );
 }
