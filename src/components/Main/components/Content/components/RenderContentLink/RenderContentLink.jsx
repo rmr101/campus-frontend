20bded91905a693e6c4a79c229d852be861ae2a6
jackytsheng <jackytsheng@gmail.com>
2020-07-09 20:05:58 +1000

    changes made

--- src/components/Main/components/Content/components/RenderContentLink/RenderContentLink.jsx
+++ src/components/Main/components/Content/components/RenderContentLink/RenderContentLink.jsx
@@ -7,7 +7,6 @@ import AddHeader from "./../../../../../../store/campus/actions/AddHeader";
 const renderRest = (rest) => {
     let result = [];
     for (let props in rest) {
-      console.log(rest);
       result.push(renderColumn(rest[props]));
     }
     return result;
@@ -21,27 +20,26 @@ const renderColumn = (detail) => {
   );
 };
 
-const RenderContentLink = ({ onClick, RenderObj, toPageID }) => {
-  
+const RenderContentLink = ({ onClick, RenderObj, toPageID}) => {
   const { name, id, ...rest } = RenderObj;
   renderRest(rest);
 
-    return (
-      <div key={"name_" + Math.random()} className={styles.container}>
-        <div
-          onClick={(e) => {
-            e.preventDefault();
-            onClick(name, toPageID, id);
-          }}
-          className={`${styles.links} ${styles.heading}`}
-        >
-          {name}
-        </div>
-        <div className={styles.heading}>{id}</div>
-        {renderRest(rest)}
+  return (
+    <div key={"name_" + Math.random()} className={styles.container}>
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          onClick(name, toPageID, id);
+        }}
+        className={`${styles.links} ${styles.heading}`}
+      >
+        {name}
       </div>
-    );
-  };
+      <div className={styles.heading}>{id}</div>
+      {renderRest(rest)}
+    </div>
+  );
+};
 
 
 const mapDispatchToProps = (dispatch) => ({
