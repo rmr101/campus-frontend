aa2aa591ae63877f771c18061452e28ce4d6ea3c
jackytsheng <jackytsheng@gmail.com>
2020-07-13 22:39:17 +1000

    added unit test

---
+++ src/components/Main/Main.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {render,screen,within} from '@testing-library/react';
+import Main from './Main';
+import headerHistoryReducer from '../../store/campus/reducers/headerHistory';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+
+
+const initialState = {
+  headerHistory:{
+  title:"Title",
+  content:{
+    pageID:"PageID",
+  }
+}}
+const Wrapper = ({children}) =>
+  <Provider store={createStore(headerHistoryReducer, initialState)}>
+    {children}
+  </Provider>;
+
+
+jest.mock('./components/Content', ()=> jest.fn(()=> `<Content/>` ));
+jest.mock("./components/Header", () =>
+  jest.fn(() => `<Header/>`)
+); 
+
+describe("<Main/>", ()=>{
+
+  beforeEach(()=>{
+    render(<Main/>,{wrapper:Wrapper});
+  })
+it("should render header",()=>{
+  expect(screen.getByTestId("header")).toBeInTheDocument();
+})
+it("should render Content", () => {
+  expect(screen.getByTestId("content")).toBeInTheDocument();
+});
+
+})
