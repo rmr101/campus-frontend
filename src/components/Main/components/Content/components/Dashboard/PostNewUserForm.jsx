14af14244093aeb8ae120feb082f01dfbd97e434
jackytsheng <jackytsheng@gmail.com>
2020-07-21 01:16:48 +1000

    fix warnning

--- src/components/Main/components/Content/components/Dashboard/PostNewUserForm.jsx
+++ src/components/Main/components/Content/components/Dashboard/PostNewUserForm.jsx
@@ -44,7 +44,7 @@ class PostNewUserForm extends React.Component {
   }
 
   validateEmail() {
-    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const char = this.state.email;
     console.log(char);
     if (!regex.test(char)) {
