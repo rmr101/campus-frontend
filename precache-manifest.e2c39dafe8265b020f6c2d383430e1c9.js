6809a3cc99d9fc165825edb3a3f2b0becc2679c1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-21 04:49:16 +0000

    Deploy rmr101/campus-frontend to github.com/rmr101/campus-frontend.git:gh-pages

---
+++ precache-manifest.e2c39dafe8265b020f6c2d383430e1c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d0df6765175c99e091bd8ba670cf91",
+    "url": "/campus-frontend/index.html"
+  },
+  {
+    "revision": "3f1d20d01c253998eb16",
+    "url": "/campus-frontend/static/css/main.1eb81c1d.chunk.css"
+  },
+  {
+    "revision": "0e3ba10628338c94bf4b",
+    "url": "/campus-frontend/static/js/2.08a7d92a.chunk.js"
+  },
+  {
+    "revision": "67dca3a505ab501a7bc6a707b200f3bb",
+    "url": "/campus-frontend/static/js/2.08a7d92a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1d20d01c253998eb16",
+    "url": "/campus-frontend/static/js/main.3932588e.chunk.js"
+  },
+  {
+    "revision": "1fdf0662e28ebb004160",
+    "url": "/campus-frontend/static/js/runtime-main.4f36e4c5.js"
+  },
+  {
+    "revision": "24e2fc501d69ddd0e566d50aad166fb8",
+    "url": "/campus-frontend/static/media/logo-light.24e2fc50.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc08dfae332996e6ec237309f206888e.js"
+  "/campus-frontend/precache-manifest.e2c39dafe8265b020f6c2d383430e1c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/campus-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
