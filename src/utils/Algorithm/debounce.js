7abbe4db434e36e3119f7bf4edd17c6ef1803108
jackytsheng <jackytsheng@gmail.com>
2020-07-26 22:33:59 +1000

    created debounce function, change password is now set to response code 400, edit readme.md

---
+++ src/utils/Algorithm/debounce.js
@@ -0,0 +1,15 @@
+
+// debounce function
+export default (func, wait) => {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
