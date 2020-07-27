47a27bb52f223631a116dd838907788669ad4cce
Richardxugit <cansolion@163.com>
2020-07-27 12:30:55 +1000

    fix

--- src/apis/authentication.js
+++ src/apis/authentication.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //TODO: 后端要提供一个接口给我validate 我的jwt
 
 //TODO: 以后说不定要改这个base。现在因为后端不能接受带null的authentication 额外创的。
-const baseURL = "http://192.168.0.120:8080";
+const baseURL = "http://localhost:8080";
 const AuthUrl = "/authenticate";
 
 
--- src/utils/CampusSever/CampusSever.js
+++ src/utils/CampusSever/CampusSever.js
@@ -2,7 +2,7 @@
 // baseURL can be changed later
 import axios from 'axios';
 
-const baseURL = "http://192.168.0.120:8080";
+const baseURL = "http://localhost:8080";
 const CampusSever = axios.create({
   baseURL,
 });
