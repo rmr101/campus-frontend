d805cf172136d29a098b95851c96de4a10d5cbe7
Richardxugit <cansolion@163.com>
2020-07-31 13:49:11 +1000

    debounce for search teacher

--- src/components/Popup/components/AddTeacher/AddTeacher.jsx
+++ src/components/Popup/components/AddTeacher/AddTeacher.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchTeacher from "./components/SearchTeacher";
 import SuggestionBar from "./components/SuggestionsBar";
 import getUserQuery from "../../../../apis/getUserQuery";
+import debounce from "../../../../utils/Algorithm/debounce";
 import { FormItem, InFormLayout } from "../../../Layout/FormLayout/FormLayout";
 
 class AddTeacher extends React.Component {
@@ -13,57 +14,58 @@ class AddTeacher extends React.Component {
       nameList: [],
       suggestion: "",
       errors: "",
-      uuid:null,
+      uuid: null,
     };
     this.onClick = this.onClick.bind(this);
     this.handleSearchTemp = this.handleSearchTemp.bind(this);
     this.handleSearchByTemp = this.handleSearchByTemp.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.deBounce = debounce(this.deBounce.bind(this), 200);
   }
 
-  handleSearchByTemp(searchBy){
+  handleSearchByTemp(searchBy) {
     this.setState({
       searchBy: searchBy,
     });
     console.log(this.state.searchBy);
-  };
+  }
 
-  handleClick(suggestion,uuid){
-    this.setState({
-      search: suggestion,
-      uuid,
-      errors:"",
-    },()=>console.log(this.state.search,this.state.uuid));
-    
-  };
+  handleClick(suggestion, uuid) {
+    this.setState(
+      {
+        search: suggestion,
+        uuid,
+        errors: "",
+      },
+      () => console.log(this.state.search, this.state.uuid)
+    );
+  }
 
-  handleSearchTemp(search){
+  handleSearchTemp(search) {
     let errors = this.state.errors;
     const validInputRegex = RegExp(/^[a-zA-Z0-9]+$/);
     errors = validInputRegex.test(search) ? "" : "Invalid Input!";
-    this.setState(
-      {
-        errors: errors,
-        search: search,
-      },
-      () => {
-        if (this.state.search && this.state.search.length > 1) {
-          if (this.state.search.length % 2 === 0) {
-            this.getTeacherQuery(this.state.searchBy);
-          }
-        }
-      }
-    );
+    this.setState({
+      errors: errors,
+      search: search,
+    });
+    this.deBounce(search);
     console.log(this.state.search);
-  };
+  }
 
-  async getTeacherQuery(searchBy) {
+  deBounce(search) {
+    if (search.length > 1) {
+      this.getTeacherQuery(this.state.searchBy, search);
+    }
+  }
+
+  async getTeacherQuery(searchBy, search) {
     const validTeacherIdRegex = RegExp(/^t([0-9]*)$/);
     const validTeacherNameRegex = RegExp(/^([a-zA-Z]*)$/);
     switch (searchBy) {
       case "Teacher ID":
-        if (validTeacherIdRegex.test(this.state.search) === true) {
-          const nameList = await getUserQuery("teacherId", this.state.search);
+        if (validTeacherIdRegex.test(search) === true) {
+          const nameList = await getUserQuery("teacherId", search);
           console.log(nameList);
           this.setState({
             nameList: nameList,
@@ -75,8 +77,8 @@ class AddTeacher extends React.Component {
         }
         break;
       case "Teacher Name":
-        if (validTeacherNameRegex.test(this.state.search) === true) {
-          const nameList = await getUserQuery("teacherName", this.state.search);
+        if (validTeacherNameRegex.test(search) === true) {
+          const nameList = await getUserQuery("teacherName", search);
           console.log(nameList);
           this.setState({
             nameList: nameList,
@@ -93,17 +95,17 @@ class AddTeacher extends React.Component {
   }
 
   onClick() {
-    const { handleAddTeacher,onClick } = this.props;
+    const { handleAddTeacher, onClick } = this.props;
     const { uuid, search } = this.state;
     handleAddTeacher(search, uuid);
-    
+
     //this close the popup.
     onClick();
   }
 
   render() {
-    const {search,searchBy,errors,nameList} = this.state;
-   
+    const { search, searchBy, errors, nameList } = this.state;
+
     return (
       <InFormLayout>
         <SearchTeacher
